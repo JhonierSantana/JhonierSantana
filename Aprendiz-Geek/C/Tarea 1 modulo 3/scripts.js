fc350a19e7ef283b10bff60bce0296e537f4689a
JhonierSantana <jhonier_2504@hotmail.com>
2022-04-28 17:06:00 -0500

    Se agrega Tarea 1 del modulo 3

--- Aprendiz-Geek/C/Tarea 1 modulo 3/scripts.js
+++ Aprendiz-Geek/C/Tarea 1 modulo 3/scripts.js
@@ -3,6 +3,8 @@ function calcular() {
         var a = parseInt(document.getElementById("valor1").value) || 0,
         b = parseInt(document.getElementById("valor2").value) || 0;
         document.getElementById("total").value = a * b  / 100;
+        document.getElementById("El total").innerHTML = a;
+        document.getElementById("La propina").innerHTML = a * b / 100;
     } catch (e) {}
 }
 
@@ -11,5 +13,6 @@ function suma() {
         var a = parseInt(document.getElementById("valor1").value) || 0,
         b = parseInt(document.getElementById("total").value) || 0;
         document.getElementById("Pagar").value = a + b ;
+        document.getElementById("Total a pagar").innerHTML = a + b;
     } catch (e) {}
 }
